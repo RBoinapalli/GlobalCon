6af184f47f03d135d021b604a87d00cb6afb7160
Rithika Boinapalli <rithika.boinapalli@gmail.com>
2024-11-10 22:35:04 -0500

    test2

---
+++ global-con/src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './Layout';
+import './css/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Layout />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
