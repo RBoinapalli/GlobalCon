3fdc6efc4ce23ec141326e59acb08d670bb07d58
Rithika Boinapalli <rithika.boinapalli@gmail.com>
2024-11-10 19:44:04 -0500

    add changes

---
+++ global-con/src/components/Header.js
@@ -0,0 +1,18 @@
+
+// src/components/Header.js
+import React from 'react';
+import '../css/header.css';
+import logo from '../images/logo.png';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+        <h1>Global IT Consultants</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
